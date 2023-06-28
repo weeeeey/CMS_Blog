bfc49e53bfa1969605004177292c24cd5dd31cd6
weeeeey <qser155@naver.com>
2023-06-28 20:12:25 +0900

    services GraphQL 쿼리

--- my-app/src/app/aaa/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Asds = () => {
-    return <>asdasd</>;
-};
-
-export default Asds;
--- pages/aaa.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Aaa = () => {
-    return <>asd</>;
-};
-
-export default Aaa;
