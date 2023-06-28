ab6f374e999e3cb23dfaf39b517dc2af6a0c3f94
weeeeey <qser155@naver.com>
2023-06-28 20:41:58 +0900

    post

--- services/index.ts
+++ services/index.ts
@@ -1,8 +1,48 @@
 import { request, gql } from "graphql-request";
 
+interface Author {
+    bio: string;
+    name: string;
+    id: string;
+    photo: {
+        url: string;
+    };
+}
+
+interface FeaturedImage {
+    url: string;
+}
+
+interface Category {
+    name: string;
+    slug: string;
+}
+
+interface Node {
+    author: Author;
+    createdAt: string;
+    slug: string;
+    title: string;
+    excerpt: string;
+    featuredImage: FeaturedImage;
+    categories: Category[];
+}
+
+interface Edge {
+    node: Node;
+}
+
+export interface PostsConnection {
+    edges: Edge[];
+}
+
+interface PostsConnectionResult {
+    postsConnection: PostsConnection;
+}
+
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-export const getPosts = async () => {
+export const getPosts = async (): Promise<Edge[]> => {
     const query = gql`
         query MyQuery {
             postsConnection {
@@ -32,5 +72,6 @@ export const getPosts = async () => {
             }
         }
     `;
-    const results = await request(graphqlAPI!, query);
+    const results = await request<PostsConnectionResult>(graphqlAPI!, query);
+    return results.postsConnection.edges;
 };
